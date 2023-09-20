d21b6d92065913e4b165c578b01d47c9cdefe94a
Kristina Marasovic <kristina.marasovic@croatia.rit.edu>
2023-09-20 04:33:07 +0200

    Initial file upload

---
+++ src/edu/rit/edgeconverter/Hello.java
@@ -0,0 +1,16 @@
+package edu.rit.edgeconverter;
+
+/**
+ *
+ * @author Kristina Marasovic <kristina.marasovic@croatia.rit.edu>
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("Hello, class!");
+    }
+    
+}
