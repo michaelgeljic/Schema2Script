7dba59a1f2fa7a1b2a08fd3177b1d09b6fe6fd22
zarko zizic <zz6085@rit.edu>
2025-09-21 21:14:22 +0200

    implement parser factory

---
+++ src/model/ParserFactory.java
@@ -0,0 +1,18 @@
+package model;
+
+public class ParserFactory {
+    public SchemaParser get(String format) {
+        if (format == null || format.isEmpty()) {
+            throw new IllegalArgumentException("Format cant be null or empty");
+        }
+
+        switch (format.toLowerCase()) {
+            case "xml":
+                return new XmlParser();
+            case "json":
+                return new JsonParser();
+            default:
+                throw new IllegalArgumentException("Unsupported schema format: " + format);
+        }
+    }
+}
