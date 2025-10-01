694ae9fd6b7da20280a65bff968fc6e0ec20436d
Michael Geljic <michaelgeljic@gmail.com>
2025-10-01 10:20:38 +0200

    Implemented Log4j in ParserFactory.

--- src/main/java/model/ParserFactory.java
+++ src/main/java/model/ParserFactory.java
@@ -1,9 +1,14 @@
 package main.java.model;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class ParserFactory {
+    private static final Logger logger = LogManager.getLogger();
+
     public static SchemaParser get(String format) {
         if (format == null || format.isEmpty()) {
-            throw new IllegalArgumentException("Format cant be null or empty");
+            logger.error("Schema format is null or empty");
         }
 
         switch (format.toLowerCase()) {
